3f29700a05543c9ee509fd0394e27d462eebff8d
Abhimanyu Sharma <43891700+360abhimanyu@users.noreply.github.com>
2024-05-01 16:05:43 +0530

    Delete fullstack-app-imagegallery-engine directory

--- fullstack-app-imagegallery-engine/react/src/hooks/usePokemonData.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
-
-export default function usePokemonData() {
-    return useQuery('allPokemonData', () => axios.get('/pokemon').then(res => res.data));
-}
